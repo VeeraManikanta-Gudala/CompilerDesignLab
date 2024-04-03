1cbac84c4d48cfd1082b4fa02960e2631670f11d
Veera Manikanta <gveera_it211229@mgit.ac.in>
2024-04-03 13:22:34 +0530

    week 5 programs compelted

---
+++ week2.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc < 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    char* filename = argv[1];
+    FILE* file;
+    char search[100], line[50];
+    printf("Enter the word to be searched: ");
+    scanf("%s", search);
+    // Clearing the input buffer
+    while(getchar() != '\n');
+
+    file = fopen(filename, "r");
+    if(file == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    int found = 0;
+    while(fgets(line, sizeof(line), file) != NULL)
+    {
+        if(strstr(line, search) != NULL)
+        {
+            printf("'%s' found in the line: %s", search, line);
+            found = 1;
+        }
+    }
+
+    if(!found)
+        printf("'%s' not found in the file.\n", search);
+
+    fclose(file);
+    return 0;
+}
